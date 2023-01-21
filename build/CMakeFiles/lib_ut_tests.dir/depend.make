3dc3ed0500dbe6af17b619f642e2343b1e3fd978
Martins <nwosumartins12@gmail.com>
2023-01-21 10:28:43 +0100

    Refactor: Delete generated files
    - remove generated files.
    - remove temporary objects
    - remove binaries

--- build/CMakeFiles/lib_ut_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lib_ut_tests.
-# This may be replaced when dependencies are built.
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aboycandream/dev/project_1/project_1/file_io/build/_deps/googletest-src/googletest
-# Build directory: /Users/aboycandream/dev/project_1/project_1/file_io/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
