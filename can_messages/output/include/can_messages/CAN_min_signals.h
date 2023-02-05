21b9954415979aaa8af311d939ddcefa9f8851d8
Martins <nwosumartins12@gmail.com>
2023-02-05 17:28:08 +0100

    latest changes on whole project v.6

--- can_messages/output/include/can_messages/CAN_min_signals.h
+++ can_messages/output/include/can_messages/CAN_min_signals.h
@@ -1,30 +1,32 @@
 #ifndef HEADER_MIN_SIGNALS_H
 #define HEADER_MIN_SIGNALS_H
 
-class CAN_min_signals {
-	public:
-		CAN_min_signals();
-		/*
-		 get_the ambient temperature
-		*/
-		std::string get_temperature();
-		/*
-		set the ambient temperature
-		*/
-		std::string set_temperature(float newValue);
-		/*
-		 get_the ambient humidity percentage
-		*/
-		std::string get_humidity();
-		/*
-		set the ambient humidity percentage
-		*/
-		std::string set_humidity(uint8_t newValue);
-	private:
-		uint8_t m_startMsgID;
-		uint8_t m_temperatureGetMsgID;
-		uint8_t m_temperatureSetMsgID;
-		uint8_t m_humidityGetMsgID;
-		uint8_t m_humiditySetMsgID;
+class CAN_min_signals
+{
+public:
+	CAN_min_signals();
+	/*
+	 get_the ambient temperature
+	*/
+	std::string get_temperature();
+	/*
+	set the ambient temperature
+	*/
+	std::string set_temperature(float newValue);
+	/*
+	 get_the ambient humidity percentage
+	*/
+	std::string get_humidity();
+	/*
+	set the ambient humidity percentage
+	*/
+	std::string set_humidity(uint8_t newValue);
+
+private:
+	uint8_t m_startMsgID;
+	uint8_t m_temperatureGetMsgID;
+	uint8_t m_temperatureSetMsgID;
+	uint8_t m_humidityGetMsgID;
+	uint8_t m_humiditySetMsgID;
 };
-#endif //HEADER_MIN_SIGNALS_H
+#endif // HEADER_MIN_SIGNALS_H
